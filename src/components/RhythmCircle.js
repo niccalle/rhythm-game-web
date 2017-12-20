9ec05edad4975a93b37e2465dfae5d55bf4827af
Jeong Lee <jsl133@ucsd.edu>
2017-12-20 12:08:53 -0800

    dynamic approach circle

--- src/components/RhythmCircle.js
+++ src/components/RhythmCircle.js
@@ -5,17 +5,32 @@ import PropTypes from "prop-types";
 class RhythmCircle extends React.Component {
   constructor(props) {
     super(props);
-    setInterval(() => {
-      // find out how to use this.setState() to change the approachingDistance
-    }, 100);
     this.state = {
       approachingDistance: this.props.approachingDistance
     };
   }
+  componentDidMount() {
+   this.timerID = setInterval(
+     () => this.tick(),
+     100
+   );
+ }
+
+ componentWillUnmount() {
+   clearInterval(this.timerID);
+ }
+
+  tick(){
+    this.setState({approachingDistance: this.state.approachingDistance-5});
+    if(this.state.approachingDistance <= 45){
+      clearInterval(this.timerID);
+    }
+  }
+
   render() {
     return [
       <Circle
-        radius={100}
+        radius={this.state.approachingDistance}
         x={this.props.x}
         y={this.props.y}
         fillEnabled={false}
