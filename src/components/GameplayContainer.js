5c8c208e49688010090e2592f5b078a7062185d9
Nicolas Calle <ncalle@ucsd.edu>
2017-12-20 10:06:05 -0800

    render approaching circle

--- src/components/GameplayContainer.js
+++ src/components/GameplayContainer.js
@@ -8,7 +8,7 @@ class GameplayContainer extends React.Component {
     return (
       <Stage width={700} height={700}>
         <Layer>
-          <RhythmCircle x={200} y={200} />
+          <RhythmCircle x={200} y={200} approachingDistance={100} />
         </Layer>
       </Stage>
     );
--- src/components/RhythmCircle.js
+++ src/components/RhythmCircle.js
@@ -3,16 +3,32 @@ import { Circle } from "react-konva";
 import PropTypes from "prop-types";
 
 class RhythmCircle extends React.Component {
+  constructor() {
+    super();
+    setInterval(() => {
+      // find out how to use this.setState() to change the approachingDistance
+    }, 100);
+    this.state = {
+      approachingDistance: this.props.approachingDistance
+    };
+  }
   render() {
-    return (
+    return [
+      <Circle
+        radius={100}
+        x={this.props.x}
+        y={this.props.y}
+        fillEnabled={false}
+      />,
       <Circle radius={50} x={this.props.x} y={this.props.y} fill="black" />
-    );
+    ];
   }
 }
 
 RhythmCircle.propTypes = {
   x: PropTypes.number.required,
-  y: PropTypes.number.required
+  y: PropTypes.number.required,
+  approachingDistance: PropTypes.number.required
 };
 
 export default RhythmCircle;
